dcdb2efdf05656f13a9aa2549c46114f98ae2554
meepomiracle <53315557@qq.com>
2016-06-17 18:10:47 +0800

    miracle 修正.idea目录

--- umeijia/src/main/java/com/umeijia/service/TeacherService.java
+++ umeijia/src/main/java/com/umeijia/service/TeacherService.java
@@ -1,11 +1,15 @@
 package com.umeijia.service;
 
+import com.sun.jersey.multipart.FormDataParam;
+import net.sf.json.JSONObject;
 import org.springframework.stereotype.Service;
 
-import javax.ws.rs.GET;
-import javax.ws.rs.Path;
-import javax.ws.rs.Produces;
+import javax.ws.rs.*;
 import javax.ws.rs.core.MediaType;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.InputStream;
+import java.io.OutputStream;
 
 // ip/umeijia/teacher_service/hello
 
@@ -13,11 +17,45 @@ import javax.ws.rs.core.MediaType;
 @Path("/teacher_service")
 public class TeacherService {
     
+	
 	@Path("/hello")
 	@GET
 	@Produces(MediaType.TEXT_PLAIN)
 	public String test(){
-		
-		return "welcom to UMJ server... 小峰峰";
+	
+		return "welcom to UMJ server... xiao小峰峰";
+	}
+
+	@Path("/imgUpload")
+	@POST
+	@Produces(MediaType.APPLICATION_JSON)
+	@Consumes(MediaType.MULTIPART_FORM_DATA)
+	public String imgUpload(@FormDataParam("imgData")InputStream ins, @FormDataParam("jsonArgs") String reqJson){
+        System.out.println("接收到请求...");
+		String path = "D:/work/开发/幼美加/imgs";
+		File dir = new File("D:/work/开发/幼美加/imgs");
+		if(!dir.exists()){
+			dir.mkdirs();
+            System.out.println("创建图片目录...");
+		}
+		JSONObject job = JSONObject.fromObject(reqJson);
+		JSONObject returnJsonObject = new JSONObject();
+		String imgName = job.getString("imgName");
+		File img = new File(path+"/"+imgName);
+		try {
+			OutputStream os = new FileOutputStream(img);
+			int bytesRead = 0;
+			byte[] buffer = new byte[1024];
+			while ((bytesRead = ins.read(buffer)) != -1) {
+				os.write(buffer, 0, bytesRead);
+			}
+			os.close();
+			ins.close();
+		}catch (Exception e){
+			returnJsonObject.put("status","error");
+			return returnJsonObject.toString();
+		}
+		returnJsonObject.put("status","success");
+		return returnJsonObject.toString();
 	}
 }
